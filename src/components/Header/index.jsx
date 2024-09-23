7ccea8ed7e3d9a1fa6386428fc7926d1118b047a
joao-pedro <jpmmgs@gmail.com>
2024-09-23 16:52:12 -0300

    commit for branch

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
@@ -7,6 +8,12 @@ import { Container, Profile, Logout } from "./styles";
 export function Header() {
   const { signOut, user } = useAuth();
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+  const navigation = useNavigate(); 
+
+  function handleSignOut(){
+    navigation("/");
+    signOut();
+  };
 
   return(
     <Container>
@@ -21,7 +28,7 @@ export function Header() {
           <strong>{user.name}</strong>
         </div>
       </Profile>
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
